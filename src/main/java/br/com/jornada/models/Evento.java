035e25ddd0ba692f205178d0e9a1647489a1a655
luisamaral12 <lgaa2006@gmail.com>
2016-06-15 12:49:59 -0300

    Creating basic models for our application.

---
+++ src/main/java/br/com/jornada/models/Evento.java
@@ -0,0 +1,64 @@
+package br.com.jornada.models;
+
+import java.awt.Image;
+import java.util.Calendar;
+
+public class Evento {
+	private String nome;
+	private String descricao;
+	private String site;
+	private String local;
+	private Image logo;
+	private Calendar data;
+	private Usuario organizador;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getSite() {
+		return site;
+	}
+	public void setSite(String site) {
+		this.site = site;
+	}
+	
+	public String getLocal() {
+		return local;
+	}
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	
+	public Image getLogo() {
+		return logo;
+	}
+	public void setLogo(Image logo) {
+		this.logo = logo;
+	}
+
+	public Calendar getData() {
+		return data;
+	}
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+	
+	public Usuario getOrganizador() {
+		return organizador;
+	}
+	public void setOrganizador(Usuario organizador) {
+		this.organizador = organizador;
+	}
+	
+}
---
+++ src/main/java/br/com/jornada/models/Palestra.java
@@ -0,0 +1,37 @@
+package br.com.jornada.models;
+
+public class Palestra {
+	private String titulo;
+	private String descricao;
+	private Usuario palestrante;
+	private Evento eventoDeOrigem;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public Usuario getPalestrante() {
+		return palestrante;
+	}
+	public void setPalestrante(Usuario palestrante) {
+		this.palestrante = palestrante;
+	}
+	
+	public Evento getEventoDeOrigem() {
+		return eventoDeOrigem;
+	}
+	public void setEventoDeOrigem(Evento eventoDeOrigem) {
+		this.eventoDeOrigem = eventoDeOrigem;
+	}
+	
+}
