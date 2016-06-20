8f085723548906a4f2c6328cdedaacf38b6690ed
luisamaral12 <lgaa2006@gmail.com>
2016-06-20 15:58:58 -0300

    New configurations and some tweaks

--- src/main/java/br/com/jornada/models/Usuario.java
+++ src/main/java/br/com/jornada/models/Usuario.java
@@ -1,18 +1,27 @@
 package br.com.jornada.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
 
-	private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
 	private String nome;
 	private String senha;
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
-
+	
 	public String getNome() {
 		return nome;
 	}
